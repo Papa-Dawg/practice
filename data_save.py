4d8ae428a19515969ec325ec525f27002aed861b
Papa_Dawg <ramosnathanw@gmail.com>
2025-11-10 09:20:51 +0100

    updated save function

--- data_save.py
+++ data_save.py
@@ -8,45 +8,6 @@ from colorama import Fore, Style, init
 init(autoreset=True)
 
 JSON_DATA_FILE = 'character_data.json'
-DATA_FILE = 'character_data.bin'
-
-def save_character_health(character):
-
-    save_data = f'{character.name},{character.health}\n'
-
-    updated_lines = []
-
-    found_character = False
-
-    try:
-        with open('data.txt', 'r') as f:
-            lines = f.readlines()
-    except FileNotFoundError:
-        lines = []
-    
-    for line in lines:
-        line = line.strip()
-        if not line:
-            continue
-    
-        name, _ = line.split(',')
-
-        if name == character.name:
-            updated_lines.append(save_data)
-            found_character = True
-        else:
-            updated_lines.append(line + '\n')
-    
-    if not found_character:
-        updated_lines.append(save_data)
-    
-    try:
-        with open('data.txt', 'w') as f:
-            f.writelines(updated_lines)
-            print(Fore.GREEN + Style.BRIGHT + f"\n{character.name}" + Fore.GREEN + "'s health has been saved.")
-    except Exception as e:
-        print(Fore.RED + f"Error, could not save: {e}")
-
 
 def load_character(character):
 
@@ -62,15 +23,6 @@ def load_character(character):
         return {}
     except Exception as e:
         print(Fore.RED + f"Error, could not load character from JSON file: {e}")
-    try:
-        with open(DATA_FILE, 'rb') as f:
-            return pickle.load(f)
-    except FileNotFoundError:
-        return {}
-    except EOFError:
-        return {}
-    except Exception as e:
-        print(Fore.RED + f"Error, could not load character: {e}")
 
 
 def load_all_character_data():
@@ -108,9 +60,3 @@ def save_character(character):
     except Exception as e:
         print(Fore.RED + f"Error, could not save character to JSON file: {e}")
 
-    try:
-        with open(DATA_FILE, 'wb') as f:
-            pickle.dump(all_character_data, f) 
-            print(Fore.GREEN + Style.BRIGHT + f"\n{character.name}" + Fore.GREEN + "'s stats have been saved.")
-    except Exception as e:
-        print(Fore.RED + f"Error, could not save character: {e}")
